2307180ae1ba08f9203ee41cc709250504fc39bd
DimaVorobev <dima02081991@mail.ru>
2024-12-13 15:12:57 -0800

    Added the To_do_list model and its admin panel

--- to_do_list/to_do_list_app/admin.py
+++ to_do_list/to_do_list_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import To_do_list
 
-# Register your models here.
+@admin.register(To_do_list)
+class To_do_listAdmin(admin.ModelAdmin):
+
+    """
+    Админка модели To_do_list
+    """
+    list_display=['id', 'heading', 'summary', 'term', 'status', 'created_at', 'updated_at']
+
+    @admin.display(description="Text summary")
+    def summary(self, obj) -> str:
+        """
+        Метод конвертации столбца 'description' в короткую запись
+        """
+        if len(obj.description)> 30:
+            return f"{obj.description[:20]}..."
+        return obj.description
--- to_do_list/to_do_list_app/models.py
+++ to_do_list/to_do_list_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class To_do_list(models.Model):
+    """
+    Создаем таблицу to_do_list(Список дел)
+    """
+    status_choices = [
+        ('Выполнено', 'Выполнено'),
+        ('Не выполнено', 'Не выполнено'),
+    ]
+
+    heading = models.CharField(max_length=150, verbose_name='Заголовок')
+    description = models.CharField(max_length=255, verbose_name='Описание', null=True, blank=True)
+    term = models.DateTimeField(verbose_name='Срок')
+    status = models.CharField(max_length=50, verbose_name='Статус', choices=status_choices)
+    # tag
+    created_at = models.DateTimeField(auto_now_add=True,  verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True,  verbose_name='Дата изменения')
+
+    class Meta:
+        verbose_name = 'Список дел'
+        verbose_name_plural = 'Список дел'
+
+    def __str__(self):
+        return self.heading
